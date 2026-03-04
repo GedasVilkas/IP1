49e43a32679ed9f027523440d1788ad59ef20822
GedasVilkas <gedas.vilkas@ff.stud.vu.lt>
2026-03-04 09:34:40 +0200

    IP1.cpp
    Pataisymas nr. 1

--- IP1.cpp
+++ IP1.cpp
@@ -8,31 +8,38 @@ using std::cout;
 using std::endl;
 using std::string;
 
-class roof {
+class AngleValidator{
+public:
+  static bool isValid(float angle) {
+    return angle >= 0.f && angle < 90.f;
+  }
+};
+
+class Roof {
 private:
   static int objectCount;
   static int lastId;
   int id;
   string material;
-  float angle;
   string color;
+  float angle;
 
 public:
-  roof(string material, string color) { 
-    init(material, 0.f, color); 
+  Roof(string material, string color) { 
+    init(material, color, 0.f); 
   }
-  roof(string material, float angle, string color) {
-    init(material, angle, color);
+  Roof(string material, string color, float angle) {
+    init(material, color, angle);
   }
-  ~roof() {
+  ~Roof() {
     --objectCount;
   }
 
 private:
-  void init(string material, float angle, string color) {
+  void init(string material, string color, float angle) {
     setMaterial(material);
-    this->angle = angle;
-    setColor(color);
+    this->color = color;
+    setAngle(angle);
     setId();
     ++objectCount;
   }
@@ -41,14 +48,14 @@ public:
   void setMaterial(string material) {
     this->material = material;
   }
-  void setAngle(float angle) {
-    if (angleValid(angle))
-      this->angle = angle;
-    else
-      throw std::invalid_argument("Exception in roof: angle is not valid!");
-  }
   void setColor(string color) {
     this->color = color;
+  }
+  void setAngle(float angle) {
+    if (AngleValidator::isValid(angle))
+      this->angle = angle;
+    else
+      throw std::invalid_argument("Exception in Roof: angle is not valid!");
   }
 
 private:
@@ -84,105 +91,103 @@ public:
        << getColor();
     return ss.str();
   }
-
-public:
-  static bool angleValid(float angle) {
-    return angle >= 0.f && angle < 90.f;
-  }
 };
-int roof::lastId = 0;
-int roof::objectCount = 0;
+
+int Roof::lastId = 0;
+int Roof::objectCount = 0;
 
 int main() {
   try {
-    roof r0("asphalt", "black");
-    roof *r1 = new roof("metal", 15.f, "red");
+    {
+      Roof r0("asphalt", "black");
+      Roof *r1 = new Roof("metal", "red", 15.f);
 
-    // test1
-    if (r1->getId() == 2 && 
-        r1->getLastId() == 2 && 
-        r1->getObjectCount() == 2 &&
-        r1->getMaterial() == "metal" &&
-        r1->getAngle() == 15.f &&
-        r1->getColor() == "red" &&
-        r1->toString() == "2 metal 15 red")
-      cout << "test1 passed!" << endl;
-    else
-      cout << "test1 failed!" << endl;
-
-    // test2
-    r1->setMaterial("asphalt");
-    r1->setAngle(0.f);
-    r1->setColor("black");
-
-    if (r1->getMaterial() == "asphalt" && 
-        r1->getAngle() == 0.f &&
-        r1->getColor() == "black")
-      cout << "test2 passed!" << endl;
-    else
-      cout << "test2 failed!" << endl;
-
-    // test3
-    try {
-      r1->setAngle(-10.f);
-      cout << "test3 failed!" << endl;
-    } catch (std::invalid_argument const &ex) {
-      if (!strcmp(ex.what(), "Exception in roof: angle is not valid!"))
-        cout << "test3 passed!" << endl;
+      // test1
+      if (r1->getId() == 2 && 
+          r1->getLastId() == 2 && 
+          r1->getObjectCount() == 2 &&
+          r1->getMaterial() == "metal" &&
+          r1->getColor() == "red" &&
+          r1->getAngle() == 15.f &&
+          r1->toString() == "2 metal 15 red")
+        cout << "test1 passed!" << endl;
       else
+        cout << "test1 failed!" << endl;
+
+      // test2
+      r1->setMaterial("asphalt");
+      r1->setColor("black");
+      r1->setAngle(0.f);
+
+      if (r1->getMaterial() == "asphalt" && 
+          r1->getColor() == "black" &&
+          r1->getAngle() == 0.f)
+        cout << "test2 passed!" << endl;
+      else
+        cout << "test2 failed!" << endl;
+
+      // test3
+      try {
+        r1->setAngle(-10.f);
         cout << "test3 failed!" << endl;
-    } catch (...) {
-      cout << "test3 failed!" << endl;
+      } catch (std::invalid_argument const &ex) {
+        if (!strcmp(ex.what(), "Exception in Roof: angle is not valid!"))
+          cout << "test3 passed!" << endl;
+        else
+          cout << "test3 failed!" << endl;
+      } catch (...) {
+        cout << "test3 failed!" << endl;
+      }
+
+      // test4
+      Roof *r2 = new Roof("clay", "green", 30.f);
+      Roof *r3 = new Roof("plastic", "blue", 40.f);
+
+      delete r1;
+
+      if (r3->getId() == 4 && 
+          (r3->getId() > r2->getId()) &&
+          Roof::getObjectCount() == 3)
+        cout << "test4 passed!" << endl;
+      else
+        cout << "test4 failed!" << endl;
+
+      delete r3;
+      delete r2;
     }
-
-    // test4
-    roof *r2 = new roof("clay", 30.f, "green");
-    roof *r3 = new roof("plastic", 40.f, "blue");
-
-    delete r1;
-
-    if (r3->getId() == 4 && 
-        (r3->getId() > r2->getId()) &&
-        roof::getObjectCount() == 3)
-      cout << "test4 passed!" << endl;
-    else
-      cout << "test4 failed!" << endl;
-
-    delete r3;
-    delete r2;
-  } catch (...) {
-    cout << "Unexpected error occured!" << endl;
-  }
-
-  try {
     // test5
     bool beginsZero, increasesWithNew, decreasesWithDelete, endsZero;
 
-    beginsZero = (roof::getObjectCount() == 0);
+    beginsZero = (Roof::getObjectCount() == 0);
 
-    roof *r3 = new roof("plastic", 40.f, "blue");
-    increasesWithNew = (roof::getObjectCount() == 1);
+    Roof *r0 = new Roof("plastic", "blue", 40.f);
+    increasesWithNew = (Roof::getObjectCount() == 1);
 
-    roof *r = new roof[3]{roof("asphalt", "black"),
-                          roof("metal", 15.f, "red"),
-                          roof("clay", 30.f, "green")};
-    increasesWithNew = (increasesWithNew && roof::getObjectCount() == 4);
+    Roof *r = new Roof[3]{Roof("asphalt", "black"),
+                          Roof("metal", "red", 15.f),
+                          Roof("clay", "green", 30.f)};
+    increasesWithNew = (increasesWithNew && Roof::getObjectCount() == 4);
 
     delete[] r;
-    decreasesWithDelete = (roof::getObjectCount() == 1);
+    decreasesWithDelete = (Roof::getObjectCount() == 1);
 
-    delete r3;
-    decreasesWithDelete = decreasesWithDelete && (roof::getObjectCount() == 0);
+    delete r0;
+    decreasesWithDelete = (decreasesWithDelete && Roof::getObjectCount() == 0);
 
-    endsZero = (roof::getObjectCount() == 0);
+    endsZero = (Roof::getObjectCount() == 0);
 
-    if (beginsZero && increasesWithNew && decreasesWithDelete && endsZero)
+    if (increasesWithNew && 
+        decreasesWithDelete && 
+        endsZero)
       cout << "test5 passed!" << endl;
     else
       cout << "test5 failed!" << endl;
   } catch (...) {
     cout << "Unexpected error occured!" << endl;
   }
+
+  // memory leak check
+  assert(!Roof::getObjectCount());
 
   return 0;
 }
